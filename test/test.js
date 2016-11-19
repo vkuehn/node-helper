433773b7c821c03e0308e2f58dfaf46d71fd6714
vkuehn <vkuehn@more-machines.org>
2016-11-19 21:45:46 +0100

    added runScript function as child_process fork implementation

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
+//TODO realy do some tesing with asserts
 var helper = require('../lib/node-helper.js');
 
-helper.log('test');
+//helper.log('test');
 
-helper.log(helper.getLocalIPs());
+//helper.log(helper.getLocalIPs());
+
+var rs = new helper.RunScript();
+rs.start('../test/forkMe.js');
