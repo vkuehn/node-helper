1edff823d4a8fda3552e215758ee4e1e507ae44e
vkuehn <vkuehn@more-machines.org>
2017-02-24 13:23:11 +0100

    removed log as debug library does a better job. some linting

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": ["error", 2],
+        "no-tabs": 0,
+        "no-console": "off",
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
